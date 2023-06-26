6a16f22f30ebf32c8334c5a331b93c40e7d34eda
calelin <Jiale Lin>
2023-06-25 22:08:31 -0700

    add enum type

--- packages/svelte/src/compiler/compile/nodes/shared/Node.js
+++ packages/svelte/src/compiler/compile/nodes/shared/Node.js
@@ -2,7 +2,7 @@
  * @template {string} Type
  * @template {import('../interfaces.js').INode} [Parent=import('../interfaces.js').INode]
  */
-export default class Node {
+ export default class Node {
 	/**
 	 * @readonly
 	 * @type {number}
@@ -58,11 +58,15 @@ export default class Node {
 	 * @param {import('../../../interfaces.js').TemplateNode} info
 	 */
 	constructor(component, parent, _scope, info) {
+		/** @type {number} */
 		this.start = info.start;
+		/** @type {number} */
 		this.end = info.end;
+		/** @type {Type} */
 		this.type = /** @type {Type} */ (info.type);
-		// this makes properties non-enumerable, which makes logging
-		// bearable. might have a performance cost. TODO remove in prod?
+
+		// This makes properties non-enumerable, which makes logging
+		// bearable. It might have a performance cost. TODO: Remove in prod?
 		Object.defineProperties(this, {
 			component: {
 				value: component
@@ -71,42 +75,58 @@ export default class Node {
 				value: parent
 			}
 		});
+
 		this.can_use_innerhtml = true;
 		this.is_static_content = true;
 	}
+
+	/**
+	 * @returns {void}
+	 */
 	cannot_use_innerhtml() {
 		if (this.can_use_innerhtml !== false) {
 			this.can_use_innerhtml = false;
 			if (this.parent) this.parent.cannot_use_innerhtml();
 		}
 	}
+
+	/**
+	 * @returns {void}
+	 */
 	not_static_content() {
 		this.is_static_content = false;
 		if (this.parent) this.parent.not_static_content();
 	}
 
-	/** @param {RegExp} selector */
+	/**
+	 * @param {RegExp} selector
+	 * @returns {Node | undefined}
+	 */
 	find_nearest(selector) {
 		if (selector.test(this.type)) return this;
 		if (this.parent) return this.parent.find_nearest(selector);
 	}
 
-	/** @param {string} name */
+	/**
+	 * @param {string} name
+	 * @returns {boolean | string | null}
+	 */
 	get_static_attribute_value(name) {
-		const attribute = this.attributes.find(
-			/** @param {import('../Attribute.js').default} attr */
-			(attr) => attr.type === 'Attribute' && attr.name.toLowerCase() === name
-		);
+		const attribute = this.attributes.find((attr) => attr.type === 'Attribute' && attr.name.toLowerCase() === name);
+
 		if (!attribute) return null;
 		if (attribute.is_true) return true;
 		if (attribute.chunks.length === 0) return '';
 		if (attribute.chunks.length === 1 && attribute.chunks[0].type === 'Text') {
-			return /** @type {import('../Text.js').default} */ (attribute.chunks[0]).data;
+			return attribute.chunks[0].data;
 		}
 		return null;
 	}
 
-	/** @param {string} type */
+	/**
+	 * @param {string} type
+	 * @returns {boolean}
+	 */
 	has_ancestor(type) {
 		return this.parent ? this.parent.type === type || this.parent.has_ancestor(type) : false;
 	}
